8286303b2b5f3f758751ea7c735e293e7c06d46d
ingrwf12 <ingrwf12@cepegra.be>
2025-08-26 09:09:45 +0200

    change items 4 item

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
   //test ajax
-  fetch('https://ingrwf12.cepegra-frontend.xyz/cockpit1/api/content/items/voyages')
+  fetch('https://ingrwf12.cepegra-frontend.xyz/cockpit1/api/content/item/voyages')
   .then(r => r.json())
   .then(r => {
     console.log(r)
-    document.querySelector('.voyage').innerText = r[0]['voyages-label']
+    document.querySelector('.voyage').innerText = r['voyages-label']
   })
